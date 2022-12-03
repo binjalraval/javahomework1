a632fa94db6803a1109bb56bb98d8ce9c5bd57ce
Binjal <bvs.raval30@gmail.com>
2022-12-03 11:57:13 +0000

    javahomework1

---
+++ pdf1.java
@@ -0,0 +1,15 @@
+package javahomework1;
+
+public class pdf1{
+    public static void main(String[] args)System.out.println("What Is Software");
+    {System.out.println("What Is Software Testing");
+        {System.out.println("What Is Manual Testing");
+            {System.out.println("Qualification Of Tester");
+                {System.out.println("Challenges Of Tester");
+                    {System.out.println("Software Development Life Cycle");
+                        {System.out.println("Software Testing Life Cycle");
+                            {System.out.println("Who Does Testing");
+                                {System.out.println("Benefits Of Software Testing");
+                                    {System.out.println("Risks of Software");
+
+}
---
+++ pdf2.java
@@ -0,0 +1,15 @@
+package javahomework1;
+
+public class pdf2 {
+public static void main(String[] args){System.out.println("Challenges with Manual Testing");
+    {System.out.println("Principles Of Software Testing");
+        {System.out.println("What Is Test Plan");
+            {System.out.println("Importance of Test Plan In Software Testing");
+                {System.out.println("Test Plan template");
+                    {System.out.println("What Is Test Scenario");
+                        {System.out.println("Why it is Important to write Test Scenario");
+                            {System.out.println("Test Scenario Template");
+                                {System.out.println("What Is Test Case");
+                                    {System.out.println("Why Do We Write Test Cases");
+                                        {System.out.println("Test Case Template");
+                                            {System.out.println("Software Testing Techniques");
